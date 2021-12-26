3941478b195305ce148911d3d7094bb55f2e0eab
mluk <mikauschkal@gmail.com>
2021-12-26 19:10:46 +0100

    init

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 build: config.py main.py timer.py gui.py utils.py
-	pyinstaller --onefile --icon=shutdown.ico --noconsole --path=venv/Lib/site-packages --name=Shutdown main.py
+	pipenv install filelock && pyinstaller --onefile --icon=shutdown.ico --noconsole --path=venv/Lib/site-packages --name=Shutdown main.py
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from filelock import FileLock
+
+import config
 from gui import Gui
 from timer import Timer
 
-timer = Timer()
-gui = Gui(timer=timer)
-
-gui.display()
+lock = FileLock(config.APP_NAME_LOCK, timeout=1)
+try:
+    with lock:
+        timer = Timer()
+        gui = Gui(timer=timer)
+        gui.display()
+except:
+    pass
